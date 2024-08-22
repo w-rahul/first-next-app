5b8cea51a53e20f2716105b635d1e857184d63ca
w-rahul <rahulguptaji1019@gmail.com>
2024-08-22 17:57:07 +0530

    Added Signup page

---
+++ app/signup/page.tsx
@@ -0,0 +1,8 @@
+import { Signup } from "@/components/Signup"
+
+export default async function User(){
+
+    return <div className="w-full h-screen bg-black text-white"> 
+        <Signup />
+    </div>
+}
