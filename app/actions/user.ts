7782bc5e0cba3462d7824412f0ef3906def196e9
w-rahul <rahulguptaji1019@gmail.com>
2024-08-22 17:57:39 +0530

    Added Server actions

---
+++ app/actions/user.ts
@@ -0,0 +1,20 @@
+"use server"
+
+import client from "@/db"
+
+export async function signup(email: string, password: string ){
+    try {
+        await client.user.create({
+            data:{
+                email : email, 
+                password : password
+            }
+        })
+
+        return true
+
+       } catch (error) {
+            console.log(error)
+            return false
+       }
+}
